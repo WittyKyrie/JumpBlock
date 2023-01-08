0e8b48ea88f6286bd55062d95f08980291f0fc92
DESKTOP-KRPFNKI\Admin <649547021@qq.com>
2023-01-08 23:31:42 +0800

    Init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class GameManager : Singleton<MonoBehaviour>
+{
+    private GameState _state;
+
+    private void Start()
+    {
+        ChangeGameState(GameState.GenerateLevel);
+    }
+
+    private void ChangeGameState(GameState newGameState){
+        _state = newGameState;
+
+        switch(newGameState){
+            case GameState.GenerateLevel:
+                break;
+            case GameState.GenerateBlock:
+                break;
+            case GameState.WaitingInput:
+                break;
+            case GameState.Moving:
+                break;
+            case GameState.Judgment:
+                break; 
+            case GameState.Win:
+                break;
+            case GameState.Lose:
+            default:
+                return;           
+        }
+    }
+    
+    private void Update()
+    {
+        if(_state != GameState.WaitingInput) return;
+
+        if(Input.GetKeyDown(KeyCode.A)) Shift(Vector2.left);
+        if(Input.GetKeyDown(KeyCode.D)) Shift(Vector2.right);
+        if(Input.GetKeyDown(KeyCode.W)) Shift(Vector2.up);
+        if(Input.GetKeyDown(KeyCode.S)) Shift(Vector2.down);
+    }
+
+    private void Shift(Vector2 left)
+    {
+        throw new System.NotImplementedException(left.ToString());
+    }
+}
+
+
+//游戏状态
+public enum GameState
+{
+    GenerateLevel,
+    GenerateBlock,
+    WaitingInput,
+    Moving,
+    Judgment,
+    Win,
+    Lose
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public bool isSleepyBlockHere;
+    public bool isObstacleHere;
+    public Vector2 Pos => transform.position;
+}
